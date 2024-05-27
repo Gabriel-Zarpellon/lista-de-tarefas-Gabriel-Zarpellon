1aad148987bbd2b1b2092df2869506248de70f96
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-05-27 14:45:29 -0300

    Criação de tags e atribuição de elementos/classes utilizando DOM

--- script.js
+++ script.js
@@ -10,3 +10,50 @@ const tasks = [
   { title: "Pagar a conta de energia", type: "Urgente" },
   { title: "Assistir a um documentário interessante", type: "Normal" },
 ];
+
+
+function renderElements(taskList) {
+
+  let ulTasks = document.querySelector('.tasks__list');
+  ulTasks.innerHTML = null;
+
+  for (i = 0; i < taskList.length; i++) {
+
+    ulTasks.append(createTaskItem(taskList[i]));
+  }
+}
+
+function createTaskItem(taskList) {
+
+  let taskItem = document.createElement('li');
+  let taskInfo = document.createElement('div');
+  let taskType = document.createElement('span');
+  let taskDescription = document.createElement('p');
+  let taskButton = document.createElement('button');
+
+  taskInfo.append(taskType, taskDescription);
+  taskItem.append(taskInfo, taskButton);
+
+  taskItem.classList.add('task__item');
+  taskInfo.classList.add('task-info__container');
+  taskType.classList.add('task-type');
+  taskDescription.innerText = taskList.title;
+  taskButton.classList.add('task__button--remove-task');
+
+  if (taskList.type == 'Urgente') {
+
+    taskType.classList.add('span-urgent');
+
+  } else if (taskList.type == 'Importante') {
+
+    taskType.classList.add('span-important');
+
+  } else {
+
+    taskType.classList.add('span-normal');
+  }
+  return taskItem;
+}
+
+renderElements(tasks);
+
